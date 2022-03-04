eb022f92247196825a55ed7599ab412b95e18e09
Agung Adityatama <agung.adit121@gmail.com>
2022-03-04 20:28:58 +0700

    tambah ch1

---
+++ chapter2-javascript/AgungAdityatama/ch1/index.js
@@ -0,0 +1,4 @@
+document.getElementById(`ch1`).addEventListener(`dblclick`, functionFirst);
+function functionFirst() {
+    document.getElementById(`tampil`).innerHTML += "Kampus Merdeka 2022 Kelas FE-7"
+}
