233d9dc38d1edd7645ef681923410518823e4dba
hafidzpisabilillah <hafidzpisabilillah60@gmail.com>
2022-03-04 20:52:20 +0700

    upload file

---
+++ chapter2-javascript/hafidzpisabilillah/index.js
@@ -0,0 +1,5 @@
+const buttontombol = document.querySelector("#tombol");
+const p = document.querySelector("p");
+buttontombol.addEventListener("dblclick", () => {
+  p.innerText = `Kampus Merdeka 2022 Kelas FE-7`;
+});
