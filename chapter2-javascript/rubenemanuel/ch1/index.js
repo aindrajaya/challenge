5fd6e205f2a2ad2f52410ff59959f3452738d0d1
Ruben Emanuel Widagdo <81973148+wid46do@users.noreply.github.com>
2022-03-04 20:03:49 +0700

    Add files via upload

---
+++ chapter2-javascript/rubenemanuel/ch1/index.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('#tombol');
+const p = document.querySelector('#paragraph');
+button.addEventListener('dblclick', () => {
+    p.innerHTML = "Kampus Merdeka 2022 Kelas FE-7"
+});
