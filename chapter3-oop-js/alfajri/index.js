01db50ff3e4d51333e44b65bd6b4c1d4debdcacc
alfajri23 <123190088@student.upnyk.ac.id>
2022-03-22 20:20:47 +0700

    ch3

---
+++ chapter3-oop-js/alfajri/index.js
@@ -0,0 +1,55 @@
+class Tumbuhan{
+    constructor(nama,jenis,tinggi){
+        this.nama = nama;
+        this.jenis =jenis;
+        this.tinggi =tinggi;
+    }
+
+    siram(){
+        this.tinggi += 1;
+    }
+}
+
+class Pisang extends Tumbuhan{
+    constructor(nama,jenis,tinggi,akar){
+        super(nama,jenis,tinggi);
+        this.akar = akar;
+    }
+}
+
+class Anggur extends Tumbuhan{
+    constructor(nama,jenis,tinggi,akar){
+        super(nama,jenis,tinggi);
+        this.akar = akar;
+    }
+}
+
+class Durian extends Tumbuhan{
+    constructor(nama,jenis,tinggi,akar){
+        super(nama,jenis,tinggi);
+        this.akar = akar;
+    }
+
+}
+
+class Mangga extends Tumbuhan{
+    constructor(nama,jenis,tinggi,akar){
+        super(nama,jenis,tinggi);
+        this.akar = akar;
+    }
+}
+
+class Rambutan extends Tumbuhan{
+    constructor(nama,jenis,tinggi,akar){
+        super(nama,jenis,tinggi);
+        this.akar = akar;
+    }
+}
+
+let Pisangs = new Pisang('pisang','buah',20,'serabut');
+let Anggurs = new Anggur('Anggur','buah',12,'serabut');
+let Durians = new Durian('Durian','buah',25,'serabut');
+let Manggas = new Mangga('Mangga','buah',22,'serabut');
+let Rambutans = new Rambutan('Rambutan','buah',12,'serabut');
+
+console.log(Rambutans);
