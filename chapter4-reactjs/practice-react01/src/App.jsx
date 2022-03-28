4afa42d936b71ff6bfc135b6f515675d2f196e46
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-03-28 20:37:39 +0700

    first commit

---
+++ chapter4-reactjs/practice-react01/src/App.jsx
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>👋Hi!, My Name is Jhon</p>{' '}
+        <a
+          className="App-link"
+          href="https://github.com/jhonmicc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Here
+        </a>{' '}
+      </header>{' '}
+    </div>
+  );
+}
+
+export default App;
