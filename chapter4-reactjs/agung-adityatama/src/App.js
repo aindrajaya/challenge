32feb5f924e371a16c3fb6d45df0bd1a60019e3f
Agung Adityatama <agung.adit121@gmail.com>
2022-03-28 20:42:00 +0700

    latihan reactjs

---
+++ chapter4-reactjs/agung-adityatama/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Halo Dunia, Salam Koding!
+        <
+        /p> <
+        a className = "App-link"
+        href = "https://github.com/AgungAdityatama"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Agung Adityatama <
+        /a> < /
+        header > <
+        /div>
+    );
+}
+
+export default App;
