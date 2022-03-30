048cb9471d376b6f169e31ae7587e12c799330dc
didapatria <didapatria3@gmail.com>
2022-03-30 21:24:15 +0700

    class component

--- chapter4-reactjs/didapatria/counter-dida/src/App.js
+++ chapter4-reactjs/didapatria/counter-dida/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ClassCounter from './pages/ClassCounter';
 import Counter from './pages/Counter';
 
 function App() {
   return (
     <div className='container'>
+      <ClassCounter />
       <Counter />
     </div>
   );
