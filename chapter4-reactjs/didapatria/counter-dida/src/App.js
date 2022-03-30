2a8ed2311dd5d1ecd55c488453954d75e5ac2b2b
didapatria <didapatria3@gmail.com>
2022-03-30 21:10:31 +0700

    functional component

---
+++ chapter4-reactjs/didapatria/counter-dida/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './pages/Counter';
+
+function App() {
+  return (
+    <div className='container'>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
