5990ca61938eae6aeb138df3ad73a6bf3158a5e9
AhmadRafly23 <pradanaahmadrafly@gmail.com>
2022-03-31 13:05:51 +0700

    add c0unter

--- chapter4-reactjs/counter/src/Counter.jsx
+++ chapter4-reactjs/counter/src/Counter.jsx
@@ -1,30 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from "react";
 
-export default class Counter extends Component {
-  constructor(props){
-    super(props);
-    //Fill this line #1
+// export default class Counter extends Component {
+//   constructor(props) {
+//     super(props);
+//     //Fill this line #1
+//     this.state = { data: 0 };
+//   }
+
+//   increment = () => {
+//     //Fill this line #2
+//     this.setState({ data: this.state.data + 1 });
+//   };
+
+//   decrement = () => {
+//     //Fill this line #3
+//     this.setState({ data: this.state.data - 1 });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <div className="box">
+//           <h1>{this.state.data}</h1>
+//         </div>
+//         <div className="boxCenter">
+//           <button className="button-5" onClick={() => this.increment()}>
+//             +
+//           </button>
+//           <button className="button-5" onClick={() => this.decrement()}>
+//             -
+//           </button>
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+function Counter() {
+  const [data, setData] = useState(0);
+
+  function increment() {
+    setData(data + 1);
   }
 
-  increment = () => {
-    //Fill this line #2
+  function decrement() {
+    setData(data - 1);
   }
-
-  decrement = () => {
-    //Fill this line #3
-  }
-
-  render() {
-    return (
-      <>
-        <div className='box'>
-          <h1>{this.props.count}</h1>
-        </div>
-        <div className='boxCenter'>
-          {/* <button className='button-5' onClick={() => this.incrementss()}>+</button> */}
-          <button className='button-5' onClick={() => this.decrement()}>-</button>
-        </div>
-      </>
-    )
-  }
+  return (
+    <>
+      <div className="box">
+        <h1>{data}</h1>
+      </div>
+      <div className="boxCenter">
+        <button className="button-5" onClick={increment}>
+          +
+        </button>
+        <button className="button-5" onClick={decrement}>
+          -
+        </button>
+      </div>
+    </>
+  );
 }
+
+export default Counter;
