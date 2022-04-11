9e23cd0596054f89c56bc5ad0838d71655df0ce8
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-04-11 14:56:46 +0700

    counter app by jhon

--- chapter5-react-state-management/context/src/Counter/CounterClass.jsx
+++ chapter5-react-state-management/context/src/Counter/CounterClass.jsx
@@ -1,54 +1,62 @@
-import React from "react";
+import React from 'react';
 
 class CounterClass extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      count: 0
-    }
+      count: 0,
+    };
 
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this)
+    this.reset = this.reset.bind(this);
 
     //using props
-    this.incrementUsingProps = this.incrementUsingProps.bind(this)
+    this.incrementUsingProps = this.incrementUsingProps.bind(this);
+    this.decrementUsingProps = this.decrementUsingProps.bind(this);
   }
 
-  increment(){
-    this.setState(
-      {count: this.state.count + 1}
-    );
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  decrement() {
+    this.setState({
+      count: this.state.count - 1,
+    });
   }
 
-  incrementUsingProps(){
-    const {max, step} = this.props
+  incrementUsingProps() {
+    const { max, step } = this.props;
     this.setState((c) => {
-      if(c.count >= max) return;
-      return {count: c.count + step}
-    })
+      if (c.count >= max) return;
+      return { count: c.count + step };
+    });
   }
 
-  decrement(){
+  decrementUsingProps() {
     //Tambahkan batasan count === min, menggunakan props
-    this.setState({count: this.state.count - 1});
+    const { min, step } = this.props;
+    this.setState((c) => {
+      if (c.count === min) return;
+      return { count: this.state.count - step };
+    });
   }
 
-  reset(){
-    this.setState({count: 0});
+  reset() {
+    this.setState({ count: 0 });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="Counter">
         <p className="count">{this.state.count}</p>
         <section className="controls">
           <button onClick={this.incrementUsingProps}>Increment</button>
-          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.decrementUsingProps}>Decrement</button>
           <button onClick={this.reset}>Reset</button>
         </section>
       </div>
-    )
+    );
   }
 }
 
