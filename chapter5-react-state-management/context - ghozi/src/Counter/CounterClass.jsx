0cce102a96b791468971f9512cfbe95a2da3ccdb
ghozi <ghozihaq2017@gmail.com>
2022-04-11 16:52:48 +0700

    tugas awal chapter 5

---
+++ chapter5-react-state-management/context - ghozi/src/Counter/CounterClass.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+class CounterClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+
+    //using props
+    this.incrementUsingProps = this.incrementUsingProps.bind(this);
+    this.decrementUsingProps = this.decrementUsingProps.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  incrementUsingProps() {
+    const { max, step } = this.props;
+    this.setState((c) => {
+      if (c.count >= max) return;
+      return { count: c.count + step };
+    });
+  }
+
+  decrement() {
+    //Tambahkan batasan count === min, menggunakan props
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  decrementUsingProps() {
+    const { min, step } = this.props;
+    this.setState((a) => {
+      if (a.count <= min) return;
+      return { count: a.count - step };
+    });
+  }
+
+  reset() {
+    this.setState({ count: 0 });
+  }
+
+  render() {
+    return (
+      <div className="Counter">
+        <p className="count">{this.state.count}</p>
+        <section className="controls">
+          <button onClick={this.incrementUsingProps}>Increment</button>
+          <button onClick={this.decrementUsingProps}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default CounterClass;
