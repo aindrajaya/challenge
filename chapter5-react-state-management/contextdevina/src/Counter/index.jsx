02fc5241ed6aadea9dfd0b79e12fe245c5b93cfd
Cassiopeia316 <dvinagunawan@gmail.com>
2022-04-11 21:05:21 +0700

    counter function

--- chapter5-react-state-management/contextdevina/src/Counter/index.jsx
+++ chapter5-react-state-management/contextdevina/src/Counter/index.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import './style.scss'
 
 //Component
-import CounterClass from "./CounterClass";
-// import CounterFunc from "./CounterFunc";
+// import CounterClass from "./CounterClass";
+import CounterFunc from "./CounterFunc";
 
-const itemRemaining = 5;
+// const itemRemaining = 5;
 
 const CounterApp = () => {
   return(
     <main className="Application">
       <section className="Counters">
-        <CounterClass max={itemRemaining} min={0} step={1}/>
-        {/* <CounterFunc /> */}
+        {/* <CounterClass max={itemRemaining} min={0} step={1}/> */}
+        <CounterFunc />
       </section>
     </main>
   )
