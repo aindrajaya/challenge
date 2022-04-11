ae10c69f852d0fbbcc12b3c84197672239f742f1
Anis Maulidatur Rizqiyah <anismaulidah16@gmail.com>
2022-04-11 14:57:40 +0700

    set state anis

--- chapter5-react-state-management/context/src/Counter/CounterClass.jsx
+++ chapter5-react-state-management/context/src/Counter/CounterClass.jsx
@@ -1,14 +1,62 @@
 import React from "react";
 
 class CounterClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this)
+
+    this.incrementUsingProps = this.incrementUsingProps.bind(this);
+    this.decrementUsingProps = this.decrementUsingProps.bind(this);
+  }
+
+  increment() {
+    this.setState(
+      {count: this.state.count + 1}
+      );
+  }
+
+  incrementUsingProps(){
+    const {max, step} = this.props
+    this.setState((c) => {
+      if(c.count >= max) return;
+      return{count: c.count + step};
+    });
+  }
+
+  decrement() {
+    this.setState(
+      {count: this.state.count - 1}
+      );
+  }
+
+  decrementUsingProps(){
+    const {min, step} = this.props
+    this.setState((c) => {
+      if(c.count <= min) return;
+      return{count: c.count - step};
+    });
+  }
+
+  reset(){
+    this.setState(
+      {count: 0}
+      );
+  }
+
   render(){
     return(
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{this.state.count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.incrementUsingProps}>Increment</button>
+          <button onClick={this.decrementUsingProps}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     )
--- chapter5-react-state-management/context/src/Counter/index.jsx
+++ chapter5-react-state-management/context/src/Counter/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import './styles.scss'
+import './style.scss'
 
 //Component
 import CounterClass from "./CounterClass";
 // import CounterFunc from "./CounterFunc";
 
+const itemRemaining = 5;
+
 const CounterApp = () => {
   return(
     <main className="Application">
       <section className="Counters">
-        <CounterClass />
+        <CounterClass max={itemRemaining} min={0} step={1} />
         {/* <CounterFunc /> */}
       </section>
     </main>
