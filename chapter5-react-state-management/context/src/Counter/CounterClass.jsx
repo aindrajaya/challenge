0e0cfb454bbd46349f1d40faadffa05876b70dcc
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-04-11 14:46:25 +0700

    update decrement using props kevin

--- chapter5-react-state-management/context/src/Counter/CounterClass.jsx
+++ chapter5-react-state-management/context/src/Counter/CounterClass.jsx
@@ -1,14 +1,59 @@
 import React from "react";
 
 class CounterClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+
+    // using props
+    this.incrementUsingProp = this.incrementUsingProp.bind(this)
+    this.decrementUsingProp = this.decrementUsingProp.bind(this)
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  reset() {
+    this.setState({ count: 0 });
+  }
+
+  incrementUsingProp() {
+    // destructuring props
+    const { max, step} = this.props;
+    this.setState((counter) => { //counter menggantikan state yang ada di constructor
+      if(counter.count >= max) return; 
+      return { count: counter.count + step }
+    })
+  }
+
+  decrementUsingProp() {
+    // destructuring props
+    const { step, min } = this.props;
+    this.setState((counter) => {
+      if (counter.count <= min) return;
+      return { count: counter.count - step }
+    })
+  }
+
   render(){
     return(
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{this.state.count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.incrementUsingProp}>Increment</button>
+          <button onClick={this.decrementUsingProp}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     )
