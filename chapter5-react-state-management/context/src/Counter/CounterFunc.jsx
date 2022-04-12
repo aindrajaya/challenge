96625afba3b819300d59a09a99703d8562eb6297
shohibun <shohibunnajam@gmail.com>
2022-04-12 14:18:05 +0700

    pnambahan decrement di counterFuntion

--- chapter5-react-state-management/context/src/Counter/CounterFunc.jsx
+++ chapter5-react-state-management/context/src/Counter/CounterFunc.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 
-const CounterFunc = () => {
+const CounterFunc = ({max, step, min}) => {
   const [count, setCount] = React.useState(0)
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const increment = () => {
+    if (count >= max) return alert("Sudah maximal");
+    return setCount(count + 1);
+  } 
+
+  const decrement = () => {
+    if (count <= min) return alert("Sudah minimal");
+    return setCount(count - 1);
+  } 
+
   const reset = () => setCount(0);
+
+  // Side effect, adalah proses lain yang dijalankan selain proses utama
+  React.useEffect(() => {
+    document.title = `Your counter is: ${count}`; // componentDidMount
+  }, [count]) // depedencies, merupakan suatu pengubah 
 
   return(
     <div className="Counter">
-      <p className="count">0</p>
+      <p className="count">{count}</p>
       <section className="controls">
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
