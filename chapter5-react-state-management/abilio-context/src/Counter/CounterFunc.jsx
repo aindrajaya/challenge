8eefb12ed7b7894cd21ba206653c891ce89c16bd
Abilio Krismanuel <78332696+abiliokrismanuel@users.noreply.github.com>
2022-04-12 14:48:19 +0700

    class and func compo incre decre

---
+++ chapter5-react-state-management/abilio-context/src/Counter/CounterFunc.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const CounterFunc = ({max, step, min}) => {
+  const [count, setCount] = React.useState(0)
+  // const [count, setCount] = useState(0)
+
+  const increment = () =>  {
+    if(count >= max) return alert("item habis");
+    return setCount(count + step)
+  }
+
+  //Side effect, adalah proses lain yang di jalankan selain proses utama
+  React.useEffect(() => {
+    //componentDidMount -> untuk update title, sesuai dengan state count
+    document.title = `${count}`
+  }, [count]) //dependencies, merupaka sesuatu pengubah
+  
+  const decrement = () =>  setCount(count - 1)
+  const reset = () => setCount(0)
+
+  return(
+    <div className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  )
+}
+
+export default CounterFunc;
