19d8b046de00bea6c1033cc29c3825a107b9540a
wid46do <rubenemmanuel2000w@gmail.com>
2022-04-13 14:36:00 +0700

    light mode and dark mode

--- chapter5-react-state-management/context-ruben/src/App.js
+++ chapter5-react-state-management/context-ruben/src/App.js
@@ -1,9 +1,39 @@
+import { createContext, useState } from 'react';
 import CounterApp from './Counter';
+import ReactSwitch from 'react-switch';
+
+//Style
+import './Theme.css';
+import Form from './Form';
+
+//Declare component context
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  };
+
+  if(theme === "dark"){
+    document.body.classList.add("dark-theme");
+  } else{
+    document.body.classList.remove("dark-theme")
+  }
+
   return (
-    <CounterApp />
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div className='App' id={theme}>
+        <h2>Ruben Emanuel Widagdo</h2>
+        <Form />
+        <div className='switch'>
+          <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "light"}/>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
-}
+};
 
 export default App;
--- chapter5-react-state-management/context-ruben/src/Counter/CounterFunc.jsx
+++ chapter5-react-state-management/context-ruben/src/Counter/CounterFunc.jsx
@@ -1,16 +1,76 @@
 import React from "react";
 
+const getStateFromLocalStorage = (defaultValue, key) => {
+  const storage = localStorage.getItem(key);
+  console.log(storage);
+  if(storage) return JSON.parse(storage).count;
+  return defaultValue;
+};
+
+const storeStateInLocalStorage = (count) => {
+  localStorage.setItem("counterStorage", JSON.stringify({count}));
+  console.log(localStorage);
+};
+
+// custom hook
+const useLocalStorage = (initialState, key) => {
+  // model data
+  const getLocalStorage = () => {
+    const storage = localStorage.getItem(key)
+    console.log(storage);
+    if(storage) return JSON.parse(storage).value;
+    return initialState;
+  };
+
+  // model data
+  const getDatafromDB = async () => {
+    try {
+      const res = await fetch('your-api-endpoint')
+      const data = res.json()
+      return data
+    } catch (error) {
+      console.log(error);
+    };
+  };
+
+  //Svae value that get from local
+  const [value, setValue] = React.useState(getLocalStorage())
+  console.log("before", getLocalStorage());
+
+  //Store in local
+  React.useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({value}))
+  }, [value], console.log("After", value))
+
+  return [value, setValue]
+};
+
 const CounterFunc = ({max, step, min}) => {
   const [count, setCount] = React.useState(0);
+  console.log("before", count);
 
-  const increment = () => {
-    if(count >= max) return alert("item sudah habis");
-    return setCount(count + step);
+  // const increment = () => {
+  //   if(count >= max) return alert("item sudah habis");
+  //   return setCount(count + step);
+  // };
+
+  const increment = () =>  {
+    setCount(c => {
+      if(c >= max){
+        alert("item habis")
+        return c
+      } 
+      return c + step
+    });
   };
 
   React.useEffect(() => {
     document.title = `Your counter is: ${count}`
   }, [count]);
+
+  // React.useEffect(() => {
+  //   useLocalStorage(0, count);
+  // }, [count])
 
   const decrement = () => {
     if(count === min) return alert("item sudah habis");
