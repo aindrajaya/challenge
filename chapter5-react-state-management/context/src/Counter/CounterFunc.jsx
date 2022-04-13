e4dcc32c767d66f7ffd05769afc1a32c7824ae8f
Anis Maulidatur Rizqiyah <anismaulidah16@gmail.com>
2022-04-13 11:41:04 +0700

    dark mode anis

--- chapter5-react-state-management/context/src/Counter/CounterFunc.jsx
+++ chapter5-react-state-management/context/src/Counter/CounterFunc.jsx
@@ -1,34 +1,73 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-const CounterFunc = ({max, step, min}) => {
-  const [count, setCount] = React.useState(0)
-  // const [count, setCount] = useState(0)
-
-  const increment = () =>  {
-    if(count >= max) return alert("item sudah habis");
-    return setCount(count + step)
+// Ini Functional Component
+const CounterFunc = ({max, min, step}) => {
+  // const [count, setCount] = useState(0);
+  const getStateFromLocalStorage = (angka) => {
+    const storage = localStorage.getItem("counterStorage");
+    // console.log(storage);
+    if (storage) return JSON.parse(storage).count;
+    return localStorage.setItem("counterStorage", JSON.stringify(angka));
   }
-
-  const decrement = () =>  {
-    if(count === min) return alert("item sudah penuh");
-    return setCount(count - step)
-  }
-
-  //Side effect, adalah proses lain yang di jalankan selain proses utama
-  React.useEffect(() => {
-    //componentDidMount -> untuk update title, sesuai dengan state count
-    document.title = `Count: ${count}`
-  }, [count]) //dependencies, merupaka sesuatu pengubah
   
-  const reset = () => setCount(0)
+  const [count, setCount] = useState(getStateFromLocalStorage({ count: 0 }));
+  // console.log(count);
+
+  // side effect (efek samping) merupakan Proses lain yang dijalankan selain proses utama
+  useEffect(() => {
+    document.title = `Your counter: ${count}` //componentDidMount
+  }, [count]) //dependencies, merupakan suatu pengubah. Apabila nilai count berubah maka useEffect akan dijalankan
+  
+
+  // !Functional Component 
+  // !Menggunakan useState, belum bisa menyimpan di local storage
+  // const increment = () => setCount(count + 1);
+  // const decrement = () => setCount(count - 1);
+  // const reset = () => setCount(0);
+
+  // !Menggunakan useState, bisa menyimpan di localstorage belum ada max, min, dan step
+  // const incrementFromLocalStorage = () => {
+  //   setCount(count + 1);
+  //   localStorage.setItem("counterStorage", JSON.stringify({ count: count + 1 }));
+  // }
+
+  // const decrementFromLocalStorage = () => {
+  //   setCount(count - 1);
+  //   localStorage.setItem("counterStorage", JSON.stringify({ count: count - 1 }));
+  // }
+
+  // const resetFromLocalStorage = () => {
+  //   setCount(0);
+  //   localStorage.setItem("counterStorage", JSON.stringify({ count: 0 }));
+  // }
+
+  // !Menggunakan useState, bisa menyimpan di localstorage ada max, min, dan step
+  const incrementFromLocalStorage = () => {
+    let tambah = count + step;
+    if(count >= max) return alert("item sudah habis");
+    setCount(tambah);
+    localStorage.setItem("counterStorage", JSON.stringify({ count: tambah }));
+  }
+
+  const decrementFromLocalStorage = () => {
+    let kurang = count - step;
+    if(count <= min) return alert("item sudah habis");
+    setCount(kurang);
+    localStorage.setItem("counterStorage", JSON.stringify({ count: kurang }));
+  }
+
+  const resetFromLocalStorage = () => {
+    setCount(0);
+    localStorage.setItem("counterStorage", JSON.stringify({ count: 0 }));
+  }
 
   return(
     <div className="Counter">
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={reset}>Reset</button>
+        <button onClick={incrementFromLocalStorage}>Increment</button>
+        <button onClick={decrementFromLocalStorage}>Decrement</button>
+        <button onClick={resetFromLocalStorage}>Reset</button>
       </section>
     </div>
   )
