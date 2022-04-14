c80c91d4ff1be36aaf4f6465d3afcf126d42d092
wid46do <rubenemmanuel2000w@gmail.com>
2022-04-14 15:08:25 +0700

    add CounterRedux

--- chapter5-react-state-management/context-ruben/src/Counter/index.jsx
+++ chapter5-react-state-management/context-ruben/src/Counter/index.jsx
@@ -7,25 +7,30 @@ import CounterClass from "./CounterClass";
 import CounterFunc from "./CounterFunc";
 // import CounterUseReducer
 import CounterUseReducer from "./CounterUseReducer";
-// import CounterContext
+// import CounterContextAPI
 import CounterContextAPI from "./CounterContextApi";
 import { CounterContextProvider } from "./CounterContextApi/CounterContext";
+// import counter redux
+import CounterRedux from "./CounterRedux";
+import { Provider } from "react-redux";
+import store from "./CounterRedux/store";
 
 const itemRemaining = 5;
 
 const CounterApp = () => {
   return(
-    <CounterContextProvider>
+    <Provider store={store}>
       <main className="Application">
         <section className="Counters">
           {/* <CounterClass max={itemRemaining} min={0} step={1}/> */}
           {/* <CounterFunc max={itemRemaining} min={0} step={1}/> */}
           {/* <CounterUseReducer/> */}
-          <CounterContextAPI max={itemRemaining} min={0} step={1}/>
+          {/* <CounterContextAPI max={itemRemaining} min={0} step={1}/> */}
+          <CounterRedux max={itemRemaining} min={0} step={1}/>
         </section>
       </main>
-    </CounterContextProvider>
-  )
-}
+    </Provider>
+  );
+};
 
 export default CounterApp;
