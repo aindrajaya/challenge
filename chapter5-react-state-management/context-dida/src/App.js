73aa5e1fe839ba276eb17ee39ea7c53e5dff728a
didapatria <didapatria3@gmail.com>
2022-04-14 10:59:55 +0700

    context api

--- chapter5-react-state-management/context-dida/src/App.js
+++ chapter5-react-state-management/context-dida/src/App.js
@@ -20,7 +20,7 @@ function App() {
     <ThemeContext.Provider value={{theme, toggleTheme}}>
       <div className='App' id={theme}>
         <h2>Adinda Fadhil Patria</h2>
-        <Form />
+        <CounterApp />
         <div className='switch'>
           <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
           <ReactSwitch onChange={toggleTheme} checked={theme === "light"}/>
---
+++ chapter5-react-state-management/context-dida/src/Counter/CounterContextAPI/actions.js
@@ -0,0 +1,30 @@
+export const increment = (dispatch, max, step, count) => {
+  // console.log("data di action", max, step)
+  if(count >= max){
+    alert("barang sudah habis")
+    return count;
+  } else {
+    dispatch({
+      type: "increment",
+      payload: {step}
+    })
+  }
+}
+
+export const decrement = (dispatch, min, step, count) => {
+  if(count === min){
+    alert("item is zero")
+    return count;
+  } else {
+    dispatch({
+      type: "decrement",
+      payload: {step}
+    })
+  }
+}
+
+export const reset = (dispatch) => {
+  dispatch({
+    type: "reset",
+  })
+}
