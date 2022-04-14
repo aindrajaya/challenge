11671189ed37b7e4f4ef22dcf37fbdc59e30d0ab
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-04-14 11:09:40 +0700

    try context & useReducer

--- chapter5-react-state-management/context/src/Counter/index.jsx
+++ chapter5-react-state-management/context/src/Counter/index.jsx
@@ -3,40 +3,24 @@ import './styles.scss';
 
 //Component
 // import CounterClass from "./CounterClass";
-<<<<<<< HEAD
-import CounterFunc from './CounterFunc';
-=======
 import CounterFunc from "./CounterFunc";
 import { CounterContextProvider } from "./CounterContextAPI/counterContext";
 import CounterContextAPI from "./CounterContextAPI";
->>>>>>> 42dc2f47c17b21980693fb94e509d6717a93904f
 
 const itemRemaining = 5;
 
 const CounterApp = () => {
-<<<<<<< HEAD
   return (
-    <main className="Application">
-      <section className="Counters">
-        {/* <CounterClass max={itemRemaining} min={0} step={1}/> */}
-        <CounterFunc max={itemRemaining} min={0} step={1} />
-      </section>
-    </main>
-  );
-};
-=======
-  return(
     <CounterContextProvider>
       <main className="Application">
         <section className="Counters">
           {/* <CounterClass max={itemRemaining} min={0} step={1}/> */}
           {/* <CounterFunc max={itemRemaining} min={0} step={1}/> */}
-          <CounterContextAPI max={itemRemaining} min={0} step={1}/>
+          <CounterContextAPI max={itemRemaining} min={0} step={1} />
         </section>
       </main>
     </CounterContextProvider>
   )
 }
->>>>>>> 42dc2f47c17b21980693fb94e509d6717a93904f
 
 export default CounterApp;
