fbfa5dfaee8bb4c0b896ebec25b2cec764955168
Abilio Krismanuel <78332696+abiliokrismanuel@users.noreply.github.com>
2022-04-14 14:50:57 +0700

    Counter Using Redux

--- chapter5-react-state-management/abilio-context/src/Counter/index.jsx
+++ chapter5-react-state-management/abilio-context/src/Counter/index.jsx
@@ -6,22 +6,39 @@ import './styles.scss'
 import CounterFunc from "./CounterFunc";
 import { CounterContextProvider } from "./CounterContextAPI/counterContext";
 import CounterContextAPI from "./CounterContextAPI";
+import CounterRedux from "./CounterRedux";
+import { Provider } from "react-redux";
+import store from "./CounterRedux/store";
 
 const itemRemaining = 5;
 
+// context api
+// const CounterApp = () => {
+//   return(
+//     <CounterContextProvider>
+//     <main className="Application">
+//       <section className="Counters">
+//         {/* <CounterClass max={itemRemaining} min={0} step={1}/> */}
+
+//         {/* <CounterFunc max={itemRemaining} min={0} step={1}/> */}
+
+//         <CounterContextAPI max={itemRemaining} min={0} step={1}/>
+//       </section>
+//     </main>
+//     </CounterContextProvider>
+//   )
+// }
+
+//contex redux
 const CounterApp = () => {
   return(
-    <CounterContextProvider>
-    <main className="Application">
-      <section className="Counters">
-        {/* <CounterClass max={itemRemaining} min={0} step={1}/> */}
-
-        {/* <CounterFunc max={itemRemaining} min={0} step={1}/> */}
-
-        <CounterContextAPI max={itemRemaining} min={0} step={1}/>
-      </section>
-    </main>
-    </CounterContextProvider>
+    <Provider store={store}>
+      <main className="Application">
+        <section className="Counters">
+          <CounterRedux max={itemRemaining} min={0} step={1} />
+        </section>
+      </main>
+    </Provider>
   )
 }
 
