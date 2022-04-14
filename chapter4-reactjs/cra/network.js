448a1e8c20b96e99a1238783c1cbc83ec742a0f1
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-04-14 10:45:48 +0700

    Revert "updated"

--- chapter4-reactjs/cra/network.js
+++
@@ -1,7 +0,0 @@
-const data = async () => {
-  await fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then(json => console.log(json))
-}
-
-data()
--- chapter4-reactjs/todolist/src/TodoContextAPI/todo-context.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const TodoContext = createContext(); //membuat wadah untuk mengelola state
-
-export default TodoContext;
