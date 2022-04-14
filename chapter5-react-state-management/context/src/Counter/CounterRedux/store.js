578e22641427a1e1efd68c7c790918924093d1d9
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-04-14 14:51:44 +0700

    try redux

--- chapter5-react-state-management/context/src/Counter/CounterRedux/store.js
+++ chapter5-react-state-management/context/src/Counter/CounterRedux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
 import reducerRedux from "./reducer";
 
-// const store = createStore(reducerRedux);
+const store = createStore(reducerRedux);
 export default store
