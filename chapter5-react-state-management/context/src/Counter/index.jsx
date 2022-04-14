f83e80439a8a104da2614338e4298d25c6881ae1
arifkhrnss <arif.khairunnas@gmail.com>
2022-04-14 15:14:24 +0700

    counter redux

--- chapter5-react-state-management/context/src/Counter/index.jsx
+++ chapter5-react-state-management/context/src/Counter/index.jsx
@@ -1,38 +1,56 @@
 import React from "react";
 import './styles.scss'
 
-import CounterContextAPI from "./CounterContextAPI";
-<<<<<<< HEAD
-import { CounterContextProvider } from "./CounterContextAPI/counterContext";
-=======
-import { Provider } from "react-redux";
-import store from "./CounterRedux/store";
+//Component
+// import CounterClass from "./CounterClass";
+// import CounterFunc from "./CounterFunc";
+// import CounterContextAPI from "./CounterContextAPI";
+// import { CounterContextProvider } from "./CounterContextAPI/counterContext";
 import CounterRedux from "./CounterRedux";
->>>>>>> aindrajaya-main
+import { Provider } from "react-redux";
+import store from "./CounterRedux/store"
 
 const itemRemaining = 5;
+// Ini menggunakan Counter class dan function
+// const CounterApp = () => {
+//   return(
+//       <main className="Application">
+//         <section className="Counters">
+//           {/* <CounterClass max={itemRemaining} min={0} step={1} /> */}
+//           {/* <CounterFunc max={itemRemaining} min={0} step={1} /> */}
+//           <CounterContextAPI max={itemRemaining} min={0} step={1} />
+//         </section>
+//       </main>
+//   )
+// }
 
-// Ini menggunakan Counter Function
+
+// Ini menggunakan Counter Context API
+// const CounterApp = () => {
+//   return(
+//     <CounterContextProvider>
+//       <main className="Application">
+//         <section className="Counters">
+//           {/* <CounterClass max={itemRemaining} min={0} step={1} /> */}
+//           {/* <CounterFunc max={itemRemaining} min={0} step={1} /> */}
+//           <CounterContextAPI max={itemRemaining} min={0} step={1} />
+//         </section>
+//       </main>
+//     </CounterContextProvider>
+//   )
+// }
+// }
+
+// Ini CounterRedux
 const CounterApp = () => {
   return(
-    // <CounterContextProvider>
     <Provider store={store}>
       <main className="Application">
         <section className="Counters">
-<<<<<<< HEAD
-          <CounterContextAPI max={itemRemaining} min={0} step={1} />
-=======
-          {/* <CounterClass max={itemRemaining} min={0} step={1}/> */}
-          {/* <CounterFunc max={itemRemaining} min={0} step={1}/> */}
-          {/* <CounterContextAPI max={itemRemaining} min={0} step={1}/> */}
-          <CounterRedux max={itemRemaining} min={0} step={1}/>
->>>>>>> aindrajaya-main
+          <CounterRedux max={itemRemaining} min={0} step={1} />
         </section>
       </main>
     </Provider>
-      
-    // </CounterContextProvider>
   )
 }
-
 export default CounterApp
