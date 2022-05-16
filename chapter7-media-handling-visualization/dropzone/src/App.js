a7629fda56b55fadda76d059c81ee7285165dbd5
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-05-17 05:11:00 +0700

    add src folder #4

---
+++ chapter7-media-handling-visualization/dropzone/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+function App() {
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles);
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: 'image/png',
+  });
+
+  return (
+    <div className="App">
+      <div
+        {...getRootProps({
+          className: `dropzone 
+          ${isDragAccept && 'dropzoneAccept'} 
+          ${isDragReject && 'dropzoneReject'}`,
+        })}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
