944cef9b0e54bd5e50372e3b4bcbe349a4cf63bc
Anis Maulidatur Rizqiyah <anismaulidah16@gmail.com>
2022-05-18 20:53:51 +0700

    media handling anis

---
+++ chapter7-media-handling-visualization/dropzone-anis/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+
+function App() {
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles)
+  }, [])
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: 'img',
+  });
+
+  return (
+    <div className="App">
+      <div
+        {...getRootProps({
+          className: `dropzone
+          ${isDragAccept && 'dropzoneAccept'}
+          ${isDragReject && 'dropzoneReject'}`,
+        })}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+      <button>Import</button>
+    </div>
+  );
+}
+
+export default App;
