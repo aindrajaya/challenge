2b0f81c6efb704e99d20383fbe518aa7063e87ce
AhmadRafly23 <pradanaahmadrafly@gmail.com>
2022-05-18 20:20:47 +0700

    add dropzone

--- chapter7-media-handling-visualization/dropzone/src/App.js
+++ chapter7-media-handling-visualization/dropzone/src/App.js
@@ -1,16 +1,38 @@
-import './App.css';
-//call useCallback
-//call useDropzone
+import "./App.css";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
 function App() {
-  //declare var onDrop that called useCallback
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+  }, []);
 
-  //declare variable that call useDropzone, should be accept image
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: "image/png",
+  });
 
   return (
     <div className="App">
-      <div className="dropzone">
-        {/* Add input */}
+      <div
+        {...getRootProps({
+          className: `dropzone 
+          ${isDragAccept && "dropzoneAccept"} 
+          ${isDragReject && "dropzoneReject"}`,
+        })}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
       </div>
     </div>
   );
