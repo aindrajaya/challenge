7f7c54c4447f07971f3b50c4fcb56635664c230d
shohibun <shohibunnajam@gmail.com>
2022-05-19 18:01:12 +0700

    media handling image dan video

---
+++ chapter7-media-handling-visualization/media-handling - shohibun/src/App.js
@@ -0,0 +1,19 @@
+// import AppDropzone from "./AppDropzone";
+// import Video from "./Video";
+import Image from "./image"
+// import "./styles.css"
+
+
+function App(){
+  return(
+    <div className="App">
+      {/* <AppDropzone /> */}
+      {/* <MovieList /> */}
+      {/* <Video /> */}
+      <Image />
+    </div>
+    
+  )
+}
+
+export default App
