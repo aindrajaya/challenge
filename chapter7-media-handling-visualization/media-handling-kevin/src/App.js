0182bb3097f74e785ae65fd0efc325c4b109febf
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-05-19 15:46:34 +0700

    video preview kevin

---
+++ chapter7-media-handling-visualization/media-handling-kevin/src/App.js
@@ -0,0 +1,16 @@
+// import AppDropzone from './AppDropzone';
+
+import './App.css';
+import Video from './Video';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AppDropzone /> */}
+      {/* <MovieList /> */}
+      <Video />
+    </div>
+  );
+}
+
+export default App;
