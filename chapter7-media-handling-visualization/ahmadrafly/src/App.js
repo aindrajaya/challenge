4d6f71b88b248db5f262a4b0cb9bc52565719194
AhmadRafly23 <pradanaahmadrafly@gmail.com>
2022-05-19 15:37:29 +0700

    add video player

---
+++ chapter7-media-handling-visualization/ahmadrafly/src/App.js
@@ -0,0 +1,13 @@
+import Video from "./Video";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AppDropzone /> */}
+      {/* <MovieList /> */}
+      <Video />
+    </div>
+  );
+}
+
+export default App;
