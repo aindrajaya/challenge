b84474ce7b38db87c650542ef70cccdd4fb7d8f5
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-05-19 16:08:00 +0700

    image preview kevin

--- chapter7-media-handling-visualization/media-handling-kevin/src/Image/index.jsx
+++ chapter7-media-handling-visualization/media-handling-kevin/src/Image/index.jsx
@@ -1,31 +1,34 @@
-import React, {useEffect, useState} from "react";
-import ModalImage from "react-modal-image";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import ModalImage from 'react-modal-image';
+import axios from 'axios';
 
-import "./styles.css"
+import './styles.css';
 
 const BlogPhoto = () => {
-  const [image, setImage] = useState("");
-  const nama = "beach"
-  const clientId = "CHOQJeazu6no-4HguiJsVrcZOemdLrM7fATusMTXlnM";
-  const [photos, setPhotos] = useState([])
+  // const [image, setImage] = useState('');
+  const nama = 'beach';
+  const clientId = '1j1ClxLn5fJpzcfdUtcVsutJoty1Ft4-5rmdIHauhdw';
+  const [photos, setPhotos] = useState([]);
 
   const getData = () => {
     try {
-     axios.get(`https://api.unsplash.com/search/photos?page=1&query=beach&client_id=${clientId}`)
-     .then((res) => {
-       setPhotos(res.data.results)
-     })
+      axios
+        .get(
+          `https://api.unsplash.com/search/photos?page=1&query=${nama}&client_id=${clientId}`
+        )
+        .then((res) => {
+          setPhotos(res.data.results);
+        });
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   console.log(photos);
-  
+
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
     <div className="result">
@@ -33,16 +36,13 @@ const BlogPhoto = () => {
         <div className="AppImg">
           <p className="username"> Photo by {image.user.name}</p>
           <p className="like">👍 {image.likes}</p>
-            <div className="AppImage">
-              <ModalImage
-                small={image.urls.small}
-                large={image.urls.regular}
-              />
-            </div>
+          <div className="AppImage">
+            <ModalImage small={image.urls.small} large={image.urls.regular} />
+          </div>
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default BlogPhoto;
