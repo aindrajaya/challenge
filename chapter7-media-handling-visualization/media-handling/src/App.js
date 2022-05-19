ae3a4911bb2eed7410356ff3c0581df72fc727e0
didapatria <didapatria3@gmail.com>
2022-05-19 14:07:11 +0700

    add the video player challenge

--- chapter7-media-handling-visualization/media-handling/src/App.js
+++ chapter7-media-handling-visualization/media-handling/src/App.js
@@ -1,47 +1,17 @@
-import './App.css';
-//call useCallback
-import { useCallback } from 'react';
-//call useDropzone
-import {useDropzone} from 'react-dropzone'
+import AppDropzone from "./AppDropzone";
 
-function App() {
-  //declare var onDrop that called useCallback
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
-  }, [])
+import "./styles.css"
+import Video from "./Video";
 
-  //declare variable that call useDropzone, should be accept image
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-  } = useDropzone({
-    onDrop,
-    accept: ['image/*', 'video/*'],
-  });
-
-  return (
+function App(){
+  return(
     <div className="App">
-      <div
-        {...getRootProps({
-          className: `dropzone
-          ${isDragAccept && 'dropzoneAccept'}
-          ${isDragReject && 'dropzoneReject'}`,
-        })}
-      >
-        {/* Add input */}
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        )}
-      </div>
-      <button>Import</button>
+      {/* <AppDropzone /> */}
+      {/* <MovieList /> */}
+      <Video />
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
---
+++ chapter7-media-handling-visualization/media-handling/src/Video/VideoOverlay.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const VideoOverlay = () => (
+  <div className="video-overlay">
+    <h2>Live Event has finished</h2>
+    <p>Click the button below to restart simulation</p>
+  </div>
+);
+
+export default VideoOverlay;
