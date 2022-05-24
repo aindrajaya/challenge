20fb47c5fe848d8831d252591d1662a399adc50b
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-05-24 19:37:57 +0700

    try downgrade the package version

--- chapter7-media-handling-visualization/data-visualization/src/components/Chart.js
+++ chapter7-media-handling-visualization/data-visualization/src/components/Chart.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Bar } from "react-chartjs-2";
 
-const Chart = (chartData) => {
+const Chart = (props) => {
     return (
         <div>
             <Bar
-                data={chartData}
+                data={props.chartData}
                 options={{
                     plugins: {
                         title: {
