be30c348743ebc54b0df77b0a911fc087647b8ef
faisalkarim31 <93274214+faisalkarim31@users.noreply.github.com>
2022-05-24 20:12:52 +0700

    report

--- chapter7-media-handling-visualization/media-handling/src/Report/MovieList.jsx
+++ chapter7-media-handling-visualization/media-handling/src/Report/MovieList.jsx
@@ -14,22 +14,22 @@ const years = [
   { value: "2016", text: "2016" },
   { value: "2017", text: "2017" },
   { value: "2018", text: "2018" },
-  { value: "2019", text: "2019" }
+  { value: "2019", text: "2019" },
 ];
 
 export default function MovieList() {
   const [year, setYear] = useState("");
   const [movieDetails, setDetails] = useState([]);
-  const [show, setHide] = useState(false)
+  const [show, setHide] = useState(false);
 
-  const fetchMovie = async e => {
+  const fetchMovie = async (e) => {
     setYear(e.target.value);
     try {
       let res = await Axios(
         `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&primary_release_year=${year}&sort_by=vote_average.desc`
       );
       setDetails(res.data.results);
-      setHide(true)
+      setHide(true);
     } catch (error) {
       console.log(error);
     }
@@ -51,21 +51,23 @@ export default function MovieList() {
           );
         })}
       </select>
-      {show &&<PDFDownloadLink
-        document={<PdfDocument data={movieDetails} />}
-        fileName="movielist.pdf"
-        style={{
-          textDecoration: "none",
-          padding: "10px",
-          color: "#4a4a4a",
-          backgroundColor: "#f2f2f2",
-          border: "1px solid #4a4a4a"
-        }}
-      >
-        {({ blob, url, loading, error }) =>
-          loading ? "Loading document..." : "Download Pdf"
-        }
-      </PDFDownloadLink>}
+      {show && (
+        <PDFDownloadLink
+          document={<PdfDocument data={movieDetails} />}
+          fileName="movielist.pdf"
+          style={{
+            textDecoration: "none",
+            padding: "10px",
+            color: "#4a4a4a",
+            backgroundColor: "#f2f2f2",
+            border: "1px solid #4a4a4a",
+          }}
+        >
+          {({ blob, url, loading, error }) =>
+            loading ? "Loading document..." : "Download Pdf"
+          }
+        </PDFDownloadLink>
+      )}
     </div>
   );
 }
