c70f832b759a2078925f35df315831276a64f796
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-05-28 15:26:57 +0700

    update mini challenge chart js

--- chapter7-media-handling-visualization/data-visualization/visualization-kevin/src/App.js
+++ chapter7-media-handling-visualization/data-visualization/visualization-kevin/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Chart from './components/Chart';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const fetchPrices = async () => {
+      const res = await fetch('https://api.coincap.io/v2/assets/?limit=5');
+      const data = await res.json();
+      console.log(data);
+      setChartData({
+        labels: data.data.map((crypto) => crypto.name),
+        datasets: [
+          {
+            label: 'Price in USD',
+            data: data.data.map((crypto) => crypto.priceUsd),
+            backgroundColor: [
+              '#ffbb11',
+              '#ecf0f1',
+              '#50AF95',
+              '#f3ba2f',
+              '#2a71d0',
+            ],
+          },
+        ],
+      });
+    };
+    fetchPrices();
+  }, []);
+
+  const [chartData, setChartData] = useState({});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart chartData={chartData} />
     </div>
   );
 }
